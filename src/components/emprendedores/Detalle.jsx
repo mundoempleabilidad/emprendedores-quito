3318075f2ef0ebf243822e1f531706db4da8cf00
mundoempleabilidad <mundoempleabilidad@gmail.com>
2024-01-28 12:24:29 -0500

    Add object PDF

--- src/components/emprendedores/Detalle.jsx
+++ src/components/emprendedores/Detalle.jsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import { FaFacebook, FaWhatsapp, FaInstagram, FaTwitter } from "react-icons/fa";
 import Detailsbar from "../Detailsbar";
-
 export const Detalle = () => {
   const value = useContext(DataContext);
   const [detalle, setDetalle] = useState(null);
@@ -26,14 +25,63 @@ export const Detalle = () => {
   }, [params.name, value]);
 
   if (!detalle) {
-    return <div>Emprendedor no encontrado</div>;
+    return (
+      <>
+        <header>
+          <Detailsbar />
+        </header>
+        <div>Emprendedor no encontrado</div>;
+      </>
+    );
+  }
+
+  if (detalle.catalogo === "#") {
+    return (
+      <>
+        <header>
+          <Detailsbar />
+        </header>
+        <div className="details">
+          <Carousel>
+            {detalle.photo.map((image, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  className="d-block w-100"
+                  src={image}
+                  alt={`${detalle.name} - ${index}`}
+                />
+                <h3 className="details__name">{detalle.name}</h3>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <div className="details__description">
+            <h2>{detalle.owner}</h2>
+            <ul className="social-media details">
+              {detalle.social_links.map((link, index) => (
+                <li key={index} className="social__link">
+                  <a href={link.url} target="_blank" rel="noreferrer">
+                    {/* Utiliza el ícono correspondiente a la red social */}
+                    {link.name === "Facebook" && <FaFacebook />}
+                    {link.name === "Whatsapp" && <FaWhatsapp />}
+                    {link.name === "Instagram" && <FaInstagram />}
+                    {link.name === "Twitter" && <FaTwitter />}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <p className="details__info">{detalle.description}</p>
+          </div>
+          <div className="line__card"></div>
+        </div>
+      </>
+    );
   }
 
   return (
     <>
-    <header>
-      <Detailsbar />
-    </header>
+      <header>
+        <Detailsbar />
+      </header>
       <div className="details">
         <Carousel>
           {detalle.photo.map((image, index) => (
@@ -63,6 +111,12 @@ export const Detalle = () => {
             ))}
           </ul>
           <p className="details__info">{detalle.description}</p>
+        </div>
+        <div className="details__catalog">
+          <object
+            data={`./${detalle.catalogo}`}
+            type="application/pdf"
+          ></object>
         </div>
         <div className="line__card"></div>
       </div>
